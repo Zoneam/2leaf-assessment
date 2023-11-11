fed2d3d8aa59538627e18540f3edde83797e43de
Zoneam <zoneam@gmail.com>
2023-11-11 15:45:36 -0800

    adding backend node server to handle user signup login

---
+++ backend/models/user.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+
+const SALT_ROUNDS = 6;  
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    required: true
+  },
+  password: {
+    type: String,
+    trim: true,
+    minLength: 4,
+    required: true
+  },
+
+}, {
+  timestamps: true,
+  toJSON: {
+    transform: function(doc, ret) {
+      delete ret.password;
+      return ret;
+    }
+  }
+});
+
+userSchema.pre('save', function(next) {
+  const user = this;
+  if (!user.isModified('password')) return next();
+  bcrypt.hash(user.password, SALT_ROUNDS, function(err, hash) {
+    if (err) return next(err);
+    user.password = hash;
+    return next();
+  });
+});
+
+module.exports = mongoose.model('User', userSchema);

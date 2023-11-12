de488d93c63b0c5f103541309bf8ae36a8fa8fea
Zoneam <zoneam@gmail.com>
2023-11-12 12:59:12 -0800

    updating backend

--- frontend/dashboard.js
+++ frontend/dashboard.js
@@ -7,20 +7,17 @@ $(document).ready(function() {
             type: 'POST',
             headers: { 'Authorization': 'Bearer ' + userToken },
             success: function(response) {
-                console.log('Token is valid',response);
                 if (!response.isConfirmed) {
                     window.location.href = 'index.html';
                     alert('Please confirm your email address');
-
                 } else {
                     $('#dashboard-content').show();
                     $('#welcome-name').text(`Welcome back ${response.name}`);
                 }
             },
             error: function() {
-                console.log('Token is invalid');
-                // localStorage.removeItem('userToken');
-                // window.location.href = 'index.html';
+                localStorage.removeItem('userToken');
+                window.location.href = 'index.html';
             }
         });
     } else {
